964147d4a50445a1f0bbf2c5bd6de3529ef80f4c
mirek001 <mirek.grzegorczyk@gmail.com>
2019-03-07 20:58:01 +0100

    v0.0.8

--- Angular/build.sh
+++ Angular/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker build -f 7.3.1.dockerfile -t runserve/angular:7.3.1 .
-docker build -f 7.3.2.dockerfile -t runserve/angular:7.3.2 .
-docker build -f 7.3.3.dockerfile -t runserve/angular:7.3.3 .
-docker build -f 7.3.3.dockerfile -t runserve/angular:latest .
+# docker build -f 7.3.1.dockerfile -t runserve/angular:7.3.1 .
+# docker build -f 7.3.2.dockerfile -t runserve/angular:7.3.2 .
+# docker build -f 7.3.3.dockerfile -t runserve/angular:7.3.3 .
+docker build -f 7.3.4.dockerfile -t runserve/angular:7.3.4 .
+docker build -f 7.3.5.dockerfile -t runserve/angular:7.3.5 .
+docker build -f 7.3.5.dockerfile -t runserve/angular:latest .
--- Angular/push.sh
+++ Angular/push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker push runserve/angular:7.3.1
-docker push runserve/angular:7.3.2
-docker push runserve/angular:7.3.3
+# docker push runserve/angular:7.3.1
+# docker push runserve/angular:7.3.2
+# docker push runserve/angular:7.3.3
+docker push runserve/angular:7.3.4
+docker push runserve/angular:7.3.5
 docker push runserve/angular:latest
